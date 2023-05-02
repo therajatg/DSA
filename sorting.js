e7fea47491200060e120a3ef0dc4949abdbe5f1f
Rajat Gupta <dguptarajat@gmail.com>
2023-05-02 20:28:30 +0530

    merge sort

--- sorting.js
+++ sorting.js
@@ -1,3 +1,5 @@
+//I can use either array destructuring or a temp variable to swap. Here I used swap because then it'll be universal across all languages and probably help me in solving other DSA problems.
+
 //Assuming that the array is already sorted
 function binarySearch(arr, ele) {
   let firstIndex = 0;
@@ -45,28 +47,59 @@ function selectionSort(arr) {
         minIndex = j;
       }
     }
-
     //because no need to swap if i and min are equal
     if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
     }
   }
   return arr;
 }
 
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    for (let j = i; j > 0; j--) {
-      if (arr[j] < arr[j - 1]) {
-        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
-      } else {
-        break;
-      }
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
     }
+    array[j + 1] = temp;
   }
-  return arr;
+  return array;
 }
 
-function mergeSort() {}
+function merge(array1, array2) {
+  let combined = [];
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] < array2[j]) {
+      combined.push(array1[i]);
+      i++;
+    } else {
+      combined.push(array2[j]);
+      j++;
+    }
+  }
+  while (i < array1.length) {
+    combined.push(array1[i]);
+    i++;
+  }
+  while (j < array2.length) {
+    combined.push(array2[j]);
+    j++;
+  }
+  return combined;
+}
 
-console.log(insertionSort([23, 54, 1, 2, 78, 44, 33, 55, 66, 21]));
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+//later replace the desc
+console.log(mergeSort([23, 54, 1, 2, 78, 44, 33, 55, 66, 21]));
+// console.log(insertionSort([4, 2, 6, 5, 1, 3]));
